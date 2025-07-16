4e6729ed207aa18bedc730f7de1ad8bb4b98781f
Anuj Singh <anuj1112131415@gmail.com>
2025-07-16 13:12:24 +0530

    Add userDetails class with init block
    Introduced a userDetails class that prints name and age upon instantiation. Updated main() to create an instance of userDetails.

--- Kotlin Lang/Kotlin_Course_01/app/src/main/java/com/anujsinghdev/kotlin_course_01/kotlinPractice/21.kt
+++ Kotlin Lang/Kotlin_Course_01/app/src/main/java/com/anujsinghdev/kotlin_course_01/kotlinPractice/21.kt
@@ -1,6 +1,12 @@
 package com.anujsinghdev.kotlin_course_01.kotlinPractice
 
 fun main() {
+    val obj = userDetails("Anuj", 21)
 
+}
 
+class userDetails(name: String, age: Int) {
+    init {
+        println("My name is $name and my age is $age")
+    }
 }
