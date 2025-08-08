dcbcff492993e8d11e9ef91d5fea459cdb176e7e
Anuj Singh <anuj1112131415@gmail.com>
2025-08-08 09:27:45 +0530

    Add basic Kotlin variable and operator examples
    Added two new files demonstrating basic Kotlin variable declarations, arithmetic operations, and increment/decrement operators for educational purposes.

---
+++ Kotlin Lang/Kotlin_Course_2/app/src/main/java/com/example/kotlin_course_2/Part02/01.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_course_2.Part02
+
+fun main() {
+
+    println("What is your name?")
+
+    var number1: Int = 21
+    var number2: Short = 10000
+
+    println("Total is ${number1 + number2}")
+
+    var number3 : Float = 35.55f
+    var number4 : Double = 35.55555555555555
+
+    println("Total is ${number3 + number4}")
+
+    var bool : Boolean = true
+    var bool2 : Boolean = false
+
+
+
+}
---
+++ Kotlin Lang/Kotlin_Course_2/app/src/main/java/com/example/kotlin_course_2/Part02/02.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin_course_2.Part02
+
+fun main() {
+
+    // + - * / %
+
+    var a = 25
+    var b = 10
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+    // -- , ++
+
+    println(a--)
+    println(--a)
+    println(++a)
+
+    // = , += , -= , *= , /= , %=
+
+    
+
+
+
+}
