44e26b00d971ff97956b42690624f608895a8e34
Anuj Singh <anuj1112131415@gmail.com>
2025-08-17 15:53:03 +0530

    Add Kotlin examples for classes, abstract, and collections
    Added four new Kotlin files demonstrating class inheritance, abstract classes, interfaces, and ArrayList usage. These examples provide foundational concepts for object-oriented programming and collections in Kotlin.

---
+++ Kotlin Lang/Kotlin_Course_03/app/src/main/java/com/example/kotlin_course_03/Part1/15.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_course_03.Part1
+
+fun main() {
+    val arrayList = arrayListOf<Any>()
+    println(arrayList.size)
+    arrayList.add(4)
+    println(arrayList.size)
+
+}
+
